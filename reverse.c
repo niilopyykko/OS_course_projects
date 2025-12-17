354d34732fba8c869d5fc19f2452fc096817f4d0
Niilo <niilo.pyykko@student.lut.fi>
2025-12-17 14:44:30 +0200

    Refactor input handling in reverse.c to correctly initialize input and output file variables

--- reverse.c
+++ reverse.c
@@ -2,18 +2,27 @@
 
 int main(int argc, char *argv[])
 {
+
     if (argc == 0)
     {
         FILE *userInput = stdin;
     }
-    const char inputFile;
-    const char outputFile;
+    else if (argc == 1)
+    {
+        const char inputFile = *argv[0];
 
-    // OPEN FILE
-    FILE *readFile;
-    readFile = fopen(inputFile, "r");
+        FILE *userInput = stdin;
+    }
 
-    // READ FILE
+    else if (argc == 2)
+    {
+        const char outputFile = *argv[1];
+    }
+
+    else
+    {
+        printf("wery broken c-code ah yes");
+    }
 
     // REVERSE LINES
 
@@ -22,4 +31,16 @@ int main(int argc, char *argv[])
     WriteFile = fopen(outputFile, "w");
 
     exit(1);
+}
+
+char readFile(inputFile)
+{
+    // OPEN FILE
+    FILE *readFile;
+    readFile = fopen(inputFile, "r");
+
+    // READ FILE
+    getline(&buffer, &size, stdin);
+
+    return (0)
 }
