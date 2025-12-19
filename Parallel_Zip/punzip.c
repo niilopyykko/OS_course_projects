eb04116649bfd1f179c4215cc1f11469ed8fb902
Niilo <niilo.pyykko@student.lut.fi>
2025-12-19 19:11:41 +0200

    basic structrue derived from my-unzip and punzip

--- Parallel_Zip/punzip.c
+++ Parallel_Zip/punzip.c
@@ -4,38 +4,71 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+pthread_mutex_t output_lock = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct
 {
-    FILE *readFile;
+    const char *filename;
+} fileSpecs;
+
+void *worker(void *arg)
+{
+    fileSpecs *file = arg; // worker input back to chuck structure
+    FILE *readFile = fopen(file->filename, "rb");
+
+    if (readFile == NULL)
+    {
+        printf("punzip: cannot open file\n");
+        exit(1);
+    }
 
     int amount = 0;
     char character;
 
+    while (fread(&amount, sizeof(int), 1, readFile) &&
+           fread(&character, sizeof(char), 1, readFile))
+    {
+        pthread_mutex_lock(&output_lock);
+        for (size_t i = 0; i < amount; i++)
+        {
+            printf("%c", character);
+        }
+        pthread_mutex_unlock(&output_lock);
+    }
+    fclose(readFile);
+    return NULL;
+}
+int main(int argc, char *argv[])
+{
     if (argc < 2)
     {
         printf("my-unzip: file1 [file2 ...]\n");
         return 1;
     }
+    pthread_t workers[argc - 1]; // one thread for each file
+    fileSpecs tasks[argc - 1];
 
     for (size_t i = 1; i < argc; i++)
     {
-        readFile = fopen(argv[i], "r");
+        tasks[i - 1].filename = argv[i];
+        pthread_create(&workers[i], NULL, worker, &tasks[i]);
 
-        if (readFile == NULL)
+        for (size_t i = 0; i < argc - 1; i++)
         {
-            printf("my-unzip: cannot open file\n");
-            exit(1);
+            pthread_join(workers[i], NULL);
         }
-        while (fread(&amount, sizeof(int), 1, readFile) &&
-               fread(&character, sizeof(char), 1, readFile))
-        {
-            for (size_t i = 0; i < amount; i++)
-            {
-                printf("%c", character);
-            }
-        }
-        fclose(readFile);
     }
     return (0);
 }
